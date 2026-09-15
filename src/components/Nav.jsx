953abbfa25fb13bcf583548967232a2896898797
L1ndseyHerman <lherman21@jcu.edu>
2026-09-15 16:32:08 -0400

    Caught up thru vid 18: Responsive Mobile Menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
+import { TbShoppingBag } from "react-icons/tb";
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 const ROUTES = [
   "Home",
@@ -10,6 +12,9 @@ const ROUTES = [
 ];
 
 export function Nav() {
+  const [isMobileMenuShown, setIsMobileMenuShown] =
+    useState(false);
+
   return (
     <nav className="item-center flex flex-wrap justify-between">
       {/* Logo */}
@@ -17,19 +22,26 @@ export function Nav() {
         <NikeLogo className="h-20 w-20" />
       </a>
       {/* Burger Button (My ring looks way dif than his idk why?) */}
-      <button className="hover:bg-grey-100 focus:ring-grey-200 rounded-lg p-2 focus:ring-2">
+      <button
+        onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+        className="hover:bg-grey-100 focus:ring-grey-200 rounded-lg p-2 focus:ring-2 lg:hidden"
+      >
         <RxHamburgerMenu size={25} />
       </button>
 
       {/* Menu List */}
-      <div className="w-full">
-        <ul className="rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg">
+      <div
+        className={`${
+          isMobileMenuShown === false && "hidden"
+        } w-full lg:block lg:w-auto`}
+      >
+        <ul className="lg:space-8 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg lg:flex-row lg:border-none lg:bg-transparent">
           {ROUTES.map((route, i) => {
             return (
               <li
                 className={`cursor-pointer rounded px-3 py-2 ${
                   i === 0
-                    ? "bg-blue-500 text-white"
+                    ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500"
                     : "hover:bg-gray-100"
                 }`}
                 key={route}
@@ -39,6 +51,13 @@ export function Nav() {
             );
           })}
         </ul>
+      </div>
+
+      {/* Cart button */}
+      <div className="fixed bottom-4 left-4 lg:static">
+        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   );
