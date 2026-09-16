ce40dc7815fa53225d9c0e4e2407356f7f056f4e
L1ndseyHerman <lherman21@jcu.edu>
2026-09-16 17:10:09 -0400

    Caught up thru vid 21: Making the gradient responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,7 @@ export function Nav() {
     useState(false);
 
   return (
-    <nav className="item-center flex flex-wrap justify-between">
+    <nav className="item-center relative z-10 flex flex-wrap justify-between">
       {/* Logo */}
       <a href="#">
         <NikeLogo className="h-20 w-20" />
@@ -39,11 +39,11 @@ export function Nav() {
           {ROUTES.map((route, i) => {
             return (
               <li
-                className={`cursor-pointer rounded px-3 py-2 ${
+                className={`cursor-pointer rounded px-3 py-2 lg:hover:bg-transparent lg:hover:text-blue-500 ${
                   i === 0
                     ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500"
                     : "hover:bg-gray-100"
-                }`}
+                } ${(i === 3 || i === 4) && "lg:text-white"}`}
                 key={route}
               >
                 {route}
@@ -54,8 +54,8 @@ export function Nav() {
       </div>
 
       {/* Cart button */}
-      <div className="fixed bottom-4 left-4 lg:static">
-        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+      <div className="fixed bottom-4 left-4 lg:static lg:mr-8">
+        <div className="flex-center h-12 w-12 cursor-pointer rounded-full bg-white shadow-md">
           <TbShoppingBag />
         </div>
       </div>
