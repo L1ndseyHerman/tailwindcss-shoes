0decb370bb19d3dea2dadddf339092c2c1d66c8c
L1ndseyHerman <lherman21@jcu.edu>
2026-09-17 15:47:45 -0400

    Caught up thru vid 26: Card grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Nav } from "./components/Nav";
 import { ShoeDetail } from "./components/ShoeDetail";
-import { Card } from "./components/Card";
+import { NewArrivalsSection } from "./components/NewArrivalsSection";
 import { SHOE_LIST } from "./constant";
 
 export function App() {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      {/*<Nav />
-      <ShoeDetail />*/}
-      <Card item={SHOE_LIST[0]} />
+      <Nav />
+      <ShoeDetail />
+      <NewArrivalsSection items={SHOE_LIST} />
     </div>
   );
 }
