c4d46f036ab0f2c0c1f701a81880bd8306009e47
L1ndseyHerman <lherman21@jcu.edu>
2026-09-18 13:34:56 -0400

    Caught up thru vid 32: Dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { ShoeDetail } from "./components/ShoeDetail";
 import { NewArrivalsSection } from "./components/NewArrivalsSection";
 import { SHOE_LIST } from "./constant";
 import { Sidebar } from "./components/Sidebar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Cart } from "./components/Cart";
+import { BiSun, BiMoon } from "react-icons/bi";
 
 const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
   return {
@@ -17,6 +18,23 @@ const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
 export function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode");
+
+    if (isDarkMode === "true") {
+      window.document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  //  I had to close out of VS Code, reopen it, and npm run dev again to get this to work:
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark"),
+    );
+  };
+
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={() => setIsSidebarOpen(true)} />
@@ -28,6 +46,16 @@ export function App() {
       >
         <Cart cartItems={FAKE_CART_ITEMS} />
       </Sidebar>
+      <div className="fixed bottom-4 right-4">
+        {/* The bg-night-50 from index.css doesn't work, so I'm hardcoding it: */}
+        <button
+          onClick={toggleDarkMode}
+          className="bg-night-50 dark:text-night rounded-full bg-[#171e2c]  px-4 py-2 text-white shadow-lg dark:bg-white dark:text-[#0d1120]"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   );
 }
